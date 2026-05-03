8d2f5fadb83c8b2d5c681b3389d9878af8949ca6
Ayush Talesara <talesaraayush@gmail.com>
2026-05-03 20:19:22 +0530

    feat: implement Redis pub-sub WebSocket communication using separate producer and subscriber services

---
+++ Redis-Pub-Sub-Ws/ws1.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI ,WebSocket
+from redis.asyncio import Redis
+import uvicorn
+
+redis_client = Redis(
+    host='redis-10008.c17.us-east-1-4.ec2.cloud.redislabs.com',
+    port=10008,
+    decode_responses=True,
+    username="default",
+    password="569PS6T1nk7DFLckFattxyxA899OLxXo",
+)
+
+app = FastAPI()
+
+@app.websocket('/ws')
+async def websocket_endpoint(ws:WebSocket):
+    await ws.accept()
+    try:
+        while True:
+            data = await ws.receive_text()
+            await redis_client.publish('chat_channel', data)
+    except Exception as e:
+        print("Client disconnected", e)
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
---
+++ Redis-Pub-Sub-Ws/ws2.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI ,WebSocket
+from redis.asyncio import Redis
+import uvicorn
+import asyncio
+redis_client = Redis(
+    host='redis-10008.c17.us-east-1-4.ec2.cloud.redislabs.com',
+    port=10008,
+    decode_responses=True,
+    username="default",
+    password="569PS6T1nk7DFLckFattxyxA899OLxXo",
+)
+
+app = FastAPI()
+pubsub  = redis_client.pubsub()
+
+async def redis_receive(ws):
+        await pubsub.subscribe('chat_channel')
+
+        try:
+            async for message in pubsub.listen():
+                if message['type'] == 'message':
+                    await ws.send_text(message['data'])
+        except Exception as e:
+            print(f"Redis receive error: {e}")
+
+@app.websocket('/ws')
+async def websocket_endpoint(ws:WebSocket):
+    await ws.accept()
+    redis_task = asyncio.create_task(redis_receive(ws))
+    try:
+        while True:
+            # Wait for any incoming messages from the client to keep connection alive
+            await ws.receive_text()
+    except Exception as e:
+        print("Client disconnected", e)
+    finally:
+        redis_task.cancel()
+        await pubsub.unsubscribe('chat_channel')
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8001)
+
