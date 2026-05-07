8ebfa79dfd14a86e25afb06c6257f9fe7593cc1d
Ayush Talesara <talesaraayush@gmail.com>
2026-05-07 08:54:27 +0530

    feat: implement ForwardRequest to proxy TCP connections between client and backend servers

--- LoadBalancer/loadbalancer/main/main.go
+++ LoadBalancer/loadbalancer/main/main.go
@@ -43,6 +43,32 @@ func ForwardRequestWithoutThread(clientRequest net.Conn) error {
 
 }
 
+func ForwardRequest(clientRequest net.Conn) error {
+	fmt.Println("Forwarding request to server", CurrentServerIndex)
+	//1. select the current server
+	currentServerUrl := BackendServers[CurrentServerIndex]
+
+	connectToServer, err := net.Dial("tcp", currentServerUrl)
+	if err != nil {
+		return err
+	}
+
+	defer connectToServer.Close()
+
+	// To move to the next server after the request is served
+	CurrentServerIndex = (CurrentServerIndex + 1) % len(BackendServers)
+
+	//2. copy the response from the backend to the client
+	// io.copy is used to copy the response from the backend to the client vice versa and it take args dest and src
+	go io.Copy(connectToServer, clientRequest)
+
+	//3. copy the response from the client to the backend
+	io.Copy(clientRequest, connectToServer)
+
+	return nil
+
+}
+
 func main() {
 	//start a loa balance server
 	listner, err := net.Listen("tcp", ":8080")
@@ -60,7 +86,7 @@ func main() {
 		panic(err)
 	}
 
-	err = ForwardRequestWithoutThread(incommingRequests)
+	err = ForwardRequest(incommingRequests)
 	if err != nil {
 		fmt.Println("error forwarding request", err)
 	}
