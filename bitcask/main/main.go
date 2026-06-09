c2d9683e15a46f0f1a86ab700974576d4872a787
Ayush Talesara <talesaraayush@gmail.com>
2026-06-09 22:43:48 +0530

    feat: refactor file handling and enhance AddKeyValue logic for better data management

--- bitcask/main/main.go
+++ bitcask/main/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"hash/crc32"
 	"io"
 	"os"
 	"sort"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -16,11 +19,11 @@ type KeyMapper struct {
 }
 
 type FileInfoMapper struct {
-	FileName string
-	FileSize int64
-	FilePath string
-	ModeTime time.Time
-	IsActive bool
+	FileName     string
+	FileSize     int64
+	FileBasePath string
+	ModeTime     time.Time
+	IsActive     bool
 }
 
 type StorageLoggerObj struct {
@@ -32,9 +35,31 @@ type StorageLoggerObj struct {
 	Value        string
 }
 
+const (
+	HeaderSize  = 16
+	MaxFileSize = 1  // 1mb
+)
+
 func GenrateHash(value string) uint32 {
 	hash := crc32.ChecksumIEEE([]byte(value))
 	return hash
+}
+
+func CalculateRecordSize(key, value string) int {
+	return HeaderSize + len(key) + len(value)
+}
+
+func IncrementFileName(baseFileName *string) (string, error) {
+	updateName := strings.TrimPrefix(*baseFileName, "data-")
+	updateName = strings.TrimSuffix(updateName, ".dat")
+
+	updateNameInNum, err := strconv.Atoi(updateName)
+	if err != nil {
+		return "", err
+	}
+
+	updateName = strconv.Itoa(updateNameInNum + 1)
+	return "data-" + updateName + ".dat", nil
 }
 
 func FileLoader(baseDirPath string) ([]FileInfoMapper, error) {
@@ -59,10 +84,10 @@ func FileLoader(baseDirPath string) ([]FileInfoMapper, error) {
 		fileMapper = append(fileMapper, FileInfoMapper{
 			FileName: file.Name(),
 			// it can be dervie by filepath but for simplicity we are using file.Name() here
-			FileSize: fileInfo.Size(),
-			FilePath: baseDirPath + "/" + file.Name(),
-			ModeTime: fileInfo.ModTime(),
-			IsActive: false,
+			FileSize:     fileInfo.Size(),
+			FileBasePath: baseDirPath + "/" + file.Name(),
+			ModeTime:     fileInfo.ModTime(),
+			IsActive:     false,
 			// by deafult it will be false because we are not able to determine the active status of the file at this point
 			//, we will update it later when we will read the file and check if it is active or not
 		})
@@ -89,9 +114,19 @@ func GetFileForAppend(fileInfoMapper []FileInfoMapper) FileInfoMapper {
 	return FileInfoMapper{} // Return a default FileInfoMapper if no active file is found
 }
 
-func AddKeyValue(activeFileInfo FileInfoMapper, inMemMapper *map[string]KeyMapper, key, data string) error {
+func AddKeyValue(activeFileInfo *FileInfoMapper, inMemMapper *map[string]KeyMapper, key, data string) error {
+
+	//Step 0 check for current file size
+	if activeFileInfo.FileSize+int64(CalculateRecordSize(key, data)) >= MaxFileSize {
+		updateActiveFileName, err := IncrementFileName(&activeFileInfo.FileName)
+		if err != nil {
+			return err
+		}
+		activeFileInfo.FileName = updateActiveFileName
+	}
+
 	//Step 1 Open The File for writing the data only
-	writeFile, err := os.OpenFile(activeFileInfo.FilePath,
+	writeFile, err := os.OpenFile(activeFileInfo.FileBasePath+activeFileInfo.FileName,
 		os.O_CREATE|
 			os.O_APPEND|
 			os.O_RDWR,
@@ -107,12 +142,13 @@ func AddKeyValue(activeFileInfo FileInfoMapper, inMemMapper *map[string]KeyMappe
 
 	//Step 2 Write The Data To The File
 	appendObject := StorageLoggerObj{
-		CheckSumHash: GenrateHash(data),
+		CheckSumHash: GenrateHash(data), // 32 byte = 4 bytes
 		TimeStamp:    uint32(time.Now().Unix()),
 		KeySize:      uint32(len(key)),
 		ValueSize:    uint32(len(data)),
-		Key:          key,
-		Value:        data,
+		Key:          key,  // N bytes
+		Value:        data, // M bytes
+		// Total Fixed Size Header = 4 + 4  + 4 + 4 = 16
 	}
 	// writeLines, err := writeFile.Write([]byte(
 	// 	fmt.Sprintf("%d%d%d%d%s%s",
@@ -123,16 +159,29 @@ func AddKeyValue(activeFileInfo FileInfoMapper, inMemMapper *map[string]KeyMappe
 	// 		appendObject.Key,
 	// 		appendObject.Value,
 	// 	)))
-   
-	//#Todo need to use write bytes in code
+
+	// need to use write bytes in code because string has issue
+
+	ValueStartCursor := recordStart + int64(HeaderSize) + int64(appendObject.KeySize)
+
+	binary.Write(writeFile, binary.LittleEndian, appendObject.CheckSumHash)
+	binary.Write(writeFile, binary.LittleEndian, appendObject.TimeStamp)
+	binary.Write(writeFile, binary.LittleEndian, appendObject.KeySize)
+	binary.Write(writeFile, binary.LittleEndian, appendObject.ValueSize)
+
+	writeFile.Write([]byte(appendObject.Key))
+	writeFile.Write([]byte(appendObject.Value))
+
+	//Need To Update File Size So Rotaion Can work
+	activeFileInfo.FileSize = int64(CalculateRecordSize(key, data))
 
 	writeFile.Sync() // Ensure data is flushed to disk
 	//step 3 add to inMemMapper
 
 	(*inMemMapper)[key] = KeyMapper{
-		FileInfo:         activeFileInfo.FilePath,
+		FileInfo:         activeFileInfo.FileBasePath,
 		ValueSize:        int(appendObject.ValueSize),
-		ValueStartCursor: int(recordStart) + writeLines + int(appendObject.KeySize),
+		ValueStartCursor: int(ValueStartCursor),
 	}
 
 	return nil
@@ -163,6 +212,7 @@ func GetValueByKey(key string, inMemMapper *map[string]KeyMapper) (string, error
 
 	return string(binaryValue), nil
 }
+
 func main() {
 	fileInfoMapper, err := FileLoader("/home/ayush/Desktop/SystemDesign/bitcask/db")
 	if err != nil {
@@ -182,18 +232,18 @@ func main() {
 		defer file.Close()
 
 		newFileInfo := FileInfoMapper{
-			FileName: "data-0001.dat",
-			FileSize: 0,
-			FilePath: "/home/ayush/Desktop/SystemDesign/bitcask/db/data-0001.dat",
-			ModeTime: time.Now(),
-			IsActive: true,
+			FileName:     "data-1.dat",
+			FileSize:     0,
+			FileBasePath: "/home/ayush/Desktop/SystemDesign/bitcask/db/",
+			ModeTime:     time.Now(),
+			IsActive:     true,
 		}
 		fileInfoMapper = append(fileInfoMapper, newFileInfo)
 	}
 
 	activeFileInfo := GetFileForAppend(fileInfoMapper)
 	lookUpMapper := make(map[string]KeyMapper)
-	err = AddKeyValue(activeFileInfo, &lookUpMapper, "key1", "value1")
+	err = AddKeyValue(&activeFileInfo, &lookUpMapper, "key1", "value1")
 
 	value, err := GetValueByKey("key1", &lookUpMapper)
 	if err != nil {
